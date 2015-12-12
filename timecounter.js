ccdd60a5ba554035ac4213187ec3b19e741058de
Cheikh Sidya Camara <scicasoft@gmail.com>
2015-12-12 13:10:49 +0000

    Update timecounter.js

--- timecounter.js
+++ timecounter.js
@@ -6,43 +6,30 @@
 
 (function($){
     $.fn.extend({ 
-    
         timecounter: function(options) {
-
             var defauts={"intervalle": 1000};
-
             var params=$.extend(defauts, options);
 
             return this.each(function() {
-
                 function recharger(e) {
                     var secs = parseInt(e.attr('dsesc'))+1;
-                    
                     var texte = "";
                     
                     if (secs < 60)
                         texte = "il y'a quelques secondes";
-                        
                     else if (secs < 2*60)
                         texte = "il y'a environ 1 minute";
-                        
                     else if (secs < 60*60)
                         texte = "il y'a environ "+parseInt(secs/60)+" minutes";
-                        
                     else if (secs < 2*60*60)
                         texte = "il y'a environ 1 heure";
-                        
                     else if (secs < 24*60*60)
                         texte = "il y'a environ "+parseInt(secs/(60*60))+" heures";
-                        
                     else if (secs < 2*24*60*60)
                         texte = "hier";
-                        
                     else
                         texte = "il y'a "+parseInt(secs/(24*60*60))+" jours";
-                        
                     e.attr('dsesc', secs);
-                    
                     e.html(texte);
                 }
 
@@ -54,6 +41,5 @@
                 },params.intervalle);
             });
         }
-        
     });
 })(jQuery);
